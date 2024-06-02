ad83e547f8569c7bdfd574bd9b589b234c22c8df
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-06-02 16:41:49 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,18 @@
 import { useId } from "react";
 import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/contactsSlice";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import css from "./ContactForm.module.css";
 import { nanoid } from "nanoid";
-import { useDispatch } from "react-redux";
-import { addContact } from "../../redux/contactsSlice";
 export default function ContactForm({ onAdd }) {
+  const dispatch = useDispatch();
+  const handleAddContact = (event) => {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const number = event.target.elements.number.value;
+    dispatch(addContact({ name, number }));
+  };
   const validationSchema = Yup.object().shape({
     name: Yup.string()
       .min(3, "Too Short")
@@ -18,82 +25,42 @@ export default function ContactForm({ onAdd }) {
   });
 
   const dataId = useId();
-  const dispatch = useDispatch();
-  const handleAddContact = (event) => {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    const number = event.target.elements.number.value;
-    dispatch(addContact({ name, number }));
-  };
+
   return (
     <Formik
       initialValues={{ name: "", number: "" }}
-      onSubmit={(values) => {
-        onAdd({
-          id: nanoid(),
-          ...values,
-        });
-      }}
+      onSubmit={handleAddContact}
       validationSchema={validationSchema}
     >
-      <Form className={css.form} onSubmit={handleAddContact}>
-        <div className={css.div}>
-          <label htmlFor={dataId}>Name</label>
-          <Field
-            className={css.field}
-            type="text"
-            name="name"
-            id={dataId}
-            placeholder="Name..."
-          />
-          <ErrorMessage className={css.span} name="name" component="span" />
-        </div>
-        <div className={css.div}>
-          <label htmlFor={dataId}>Phone</label>
-          <Field
-            className={css.field}
-            type="number"
-            name="number"
-            id={dataId}
-            placeholder="Phone..."
-          />
-          <ErrorMessage className={css.span} name="number" component="span" />
-        </div>
-        <button className={css.btn} type="submit">
-          Add contact
-        </button>
-      </Form>
+      {({ handleSubmit }) => (
+        <Form className={css.form} onSubmit={handleSubmit}>
+          <div className={css.div}>
+            <label htmlFor={dataId}>Name</label>
+            <Field
+              className={css.field}
+              type="text"
+              name="name"
+              id={dataId}
+              placeholder="Name..."
+            />
+            <ErrorMessage className={css.span} name="name" component="span" />
+          </div>
+          <div className={css.div}>
+            <label htmlFor={dataId}>Phone</label>
+            <Field
+              className={css.field}
+              type="number"
+              name="number"
+              id={dataId}
+              placeholder="Phone"
+            />
+            <ErrorMessage className={css.span} name="number" component="span" />
+          </div>
+          <button className={css.btn} type="submit">
+            Add contact
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 }
-
-// export default function ContactForm() {
-
-//   return (
-//     <div>
-//       <form className={css.form} onSubmit={handleAddContact}>
-//         <div className={css.div}>
-//           <label>Name</label>
-//           <input
-//             className={css.field}
-//             type="text"
-//             name="name"
-// placeholder="Name..."
-//           />
-//         </div>
-//         <div className={css.div}>
-//           <label>Phone</label>
-//           <input
-//             className={css.field}
-//             type="number"
-//             name="number"
-//             placeholder="Phone"
-//           />
-//         </div>
-//         <button className={css.btn} type="submit">
-//           Add contact
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
